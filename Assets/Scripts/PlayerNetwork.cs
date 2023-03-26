135aba4dabc6d8a93821c7eb29c0ae793b09054f
Jaeha Yi <jaehaha1@gmail.com>
2023-03-25 18:00:19 -0700

    tested some player movement and object spawning

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,19 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class PlayerNetwork : MonoBehaviour
+public class PlayerNetwork : NetworkBehaviour
 {
+
+    [SerializeField] private Transform spawnedObjectPrefab;
+    private Transform spawnedObjectTransform; 
+
     private void Update()
     {
+        if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
+            spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true); 
+
+            //TestServerRpc(); 
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            Destroy(spawnedObjectTransform.gameObject); 
+        }
+
         Vector3 moveDir = new Vector3(0, 0, 0);
 
-        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
-        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.W)) moveDir.y = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.y = -1f;
         if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
         if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
 
         float moveSpeed = 3f;
         transform.position += moveDir * moveSpeed * Time.deltaTime; 
+    }
+
+    [ServerRpc]
+    private void TestServerRpc()
+    {
+        Debug.Log("TestServerRpc " + OwnerClientId); 
     }
 }
